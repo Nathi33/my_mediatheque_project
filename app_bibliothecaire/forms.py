28d24f347aeb266caaf23de20c05e6d53f34fd64
Nathi33 <nathi33.drnt@outlook.com>
2025-01-17 12:12:40 +0100

    add class_media

--- app_bibliothecaire/forms.py
+++ app_bibliothecaire/forms.py
@@ -3,62 +3,135 @@ from django import forms
 
 class Creationmembre(forms.Form):
     name = forms.CharField(
+        max_length=150,
         required=True,
         label="Nom",
-        error_messages={
-            'required': "Le champ 'Nom' est obligatoire"
-        }
     )
     first_name = forms.CharField(
+        max_length=150,
         required=True,
         label="Prénom",
-        error_messages={
-            'required': "Le champ 'Prénom' est obligatoire"
-        }
     )
     email = forms.EmailField(
         required=False,
         label="Email",
-        error_messages={
-            'invalid': "Veuillez entrer une adresse email valide"
-        }
     )
     phone = forms.CharField(
+        max_length=15,
         required=True,
         label="Téléphone",
-        error_messages={
-            'required': "Le champ 'Téléphone' est obligatoire"
-        }
     )
-
 
 
 class Updatemembre(forms.Form):
     name = forms.CharField(
+        max_length=150,
         required=False,
         label="Nom",
-        error_messages={
-            'required': "Le champ 'Nom' est obligatoire"
-        }
     )
     first_name = forms.CharField(
+        max_length=150,
         required=False,
         label="Prénom",
-        error_messages={
-            'required': "Le champ 'Prénom' est obligatoire"
-        }
     )
     email = forms.EmailField(
         required=False,
         label="Email",
-        error_messages={
-            'invalid': "Veuillez entrer une adresse email valide"
-        }
     )
     phone = forms.CharField(
+        max_length=15,
         required=False,
         label="Téléphone",
-        error_messages={
-            'required': "Le champ 'Téléphone' est obligatoire"
-        }
+    )
+
+
+class LivreForm(forms.Form):
+    name = forms.CharField(
+        max_length=150,
+        required=True,
+        label="Titre",
+    )
+    auteur = forms.CharField(
+        max_length=250,
+        required=True,
+        label="Auteur",
+    )
+    disponibility = forms.BooleanField(
+        initial=True,
+        required=True,
+        label="Disponible",
+    )
+    nb_pages = forms.IntegerField(
+        required=False,
+        label="Nombre de pages",
+    )
+
+
+class DvdForm(forms.Form):
+    name = forms.CharField(
+        max_length=150,
+        required=True,
+        label="Titre",
+    )
+    auteur = forms.CharField(
+        max_length=250,
+        required=True,
+        label="Réalisateur",
+    )
+    disponibility = forms.BooleanField(
+        initial=True,
+        required=True,
+        label="Disponible",
+    )
+    genre = forms.CharField(
+        required=False,
+        label="Genre",
+    )
+
+
+class CdForm(forms.Form):
+    name = forms.CharField(
+        max_length=150,
+        required=True,
+        label="Titre",
+    )
+    auteur = forms.CharField(
+        max_length=250,
+        required=True,
+        label="Artiste",
+    )
+    disponibility = forms.BooleanField(
+        initial=True,
+        required=True,
+        label="Disponible",
+    )
+    date_sortie = forms.DateField(
+        required=False,
+        label="Date de sortie"
+    )
+
+
+class PlateauForm(forms.Form):
+    name = forms.CharField(
+        max_length=150,
+        required=True,
+        label="Nom du jeu",
+    )
+    auteur = forms.CharField(
+        max_length=250,
+        required=True,
+        label="Créateur",
+    )
+    nombre_joueurs_min = forms.IntegerField(
+        required=False,
+        label="Nombre de joueurs minimum",
+    )
+    nombre_joueurs_max = forms.IntegerField(
+        required=False,
+        label="Nombre de joueurs maximum",
+    )
+    disponibility = forms.BooleanField(
+        required=False,
+        initial=False,
+        label="Disponible",
     )
--- app_membre/urls.py
+++ app_membre/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'app_membre'
+
 urlpatterns = [
     path('', views.home_membre, name='home_membre'),
 ]
